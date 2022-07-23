7830544393850b7e8d0075eb072ab336698aecc4
Brennan <merrett.b@gmail.com>
2022-07-23 14:23:32 -0400

    started User setup in graphql

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,9 +1,31 @@
+const { User, Manager } = require('../models');
+
 const resolvers = {
-    Query: {
-      helloWorld: () => {
-        return 'Hello world!';
-      }
+  Query: {
+    users: async () => {
+      return User.find()
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+    },
     }
   };
+
+// const resolvers = {
+//     Query: {
+//       users: async () => {
+//         return Manager.find()
+//         .select('-__v -password')
+//       },
+//       user: async (parent, { username }) => {
+//         return Manager.findOne({ username })
+//         .select('-__v -password')
+//       },
+//     }
+//   };
+
+
   
   module.exports = resolvers;
+
+  
--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,11 +1,30 @@
-// import the gql tagged template function
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    createdAt: String
+    email: String
+    phone: String
+    partyCount: Int
+  }
+  
+  type Manager {
+    _id: ID
+    username: String
+    password: String
+  }
+
   type Query {
-    helloWorld: String
+    users: [User]
+    user(username: String!): User
+  }
+  
+  type Mutation {
+    login: User
+    addUser(username: String!, createdAt: String!, email: String!, phone: String!, partyCount: String! ): User
   }
 `;
 
-// export the typeDefs
 module.exports = typeDefs;
