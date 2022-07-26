28e83d75be1f3d277b34c983e0bed588db895026
jasmine <jasminevirk10@gmail.com>
2022-07-26 19:34:22 -0400

    Adding Home page

--- client/src/components/HomePage.js
+++ client/src/components/HomePage.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function StartupPage() {
+export default function HomePage() {
     return (
         <div className="text-center">
             <h1 className="main-title Startup-Page title">welcome to our app</h1>
