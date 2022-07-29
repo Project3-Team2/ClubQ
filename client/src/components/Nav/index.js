f41603ba0450830312d1296680c99da862f23822
Rohitsuresh1 <99383088+Rohitsuresh1@users.noreply.github.com>
2022-07-28 22:46:18 -0400

    added logout logic and fuctioning

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -7,7 +7,13 @@ import Auth from '../../utils/auth';
 
 function Nav() {
 
+    const logout = event => {
+        event.preventDefault();
+        Auth.logout();
+    }
+    
     const loggedIn = Auth.loggedIn();
+    console.log(loggedIn);
 
     return (
         <nav style={{ display:'inline-flex', backgroundColor:'#D9594C', position: 'relative', top: 0, width: '100%', height:'36px' , alignContent:'center'}}>
@@ -15,7 +21,7 @@ function Nav() {
                 <Link to="/" style={{ textDecoration: 'none', color:'Black', display:'block', padding:'15px', fontWeight:'bold', textAlign:'center'}}> Home </Link>
                 {/* <Link to="/LandingPage" style={{ textDecoration: 'none', color:'Black', display:'block', padding:'15px', fontWeight:'bold', textAlign:'center'}}> Landing </Link> */}
                 <Link to="/RegisterPage" style={{ textDecoration: 'none', color:'Black', display:'block', padding:'15px', fontWeight:'bold', textAlign:'center'}}> Register </Link>
-                <Link to="/LogInPage" style={{ textDecoration: 'none', color:'Black', display:'block', padding:'15px', fontWeight:'bold', textAlign:'center'}}> LogIn</Link>
+                { Auth.loggedIn() ? (<a href="/" onClick={logout} style={{ textDecoration: 'none', color:'Black', display:'block', padding:'15px', fontWeight:'bold', textAlign:'center'}}> Logout </a>) : (<Link to="/LogInPage" style={{ textDecoration: 'none', color:'Black', display:'block', padding:'15px', fontWeight:'bold', textAlign:'center'}}> LogIn</Link>)}
                 {loggedIn && (<Link to="/QuePage" style={{ textDecoration: 'none', color:'Black', display:'block', padding:'15px', fontWeight:'bold', textAlign:'center'}}> View Queue</Link>)}
                 {loggedIn && (<Link to="/AdminPage" style={{ textDecoration: 'none', color:'Black', display:'block', padding:'15px', fontWeight:'bold', textAlign:'center'}}> Admin Page</Link>)}
     
