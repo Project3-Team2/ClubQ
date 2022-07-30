8cb95679c172273068d236507fff933c97cfbd59
Luyan Zhang <z709973733@gmail.com>
2022-07-29 23:28:57 -0400

    waiting page

--- client/src/components/UserQueuePage/index.js
+++ client/src/components/UserQueuePage/index.js
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import { useQuery } from "@apollo/client";
 import { CURRENT } from "../../utils/queries";
 import Auth from "../../utils/auth";
+import Nav from "../Nav";
+import Footer from "../Footer";
 
 export default function CheckPage() {
   const { data } = useQuery(CURRENT);
@@ -22,75 +24,82 @@ export default function CheckPage() {
   };
 
   const handleFormSubmit = async (event) => {
+    const formState = 
     event.preventDefault();
     try {
-      Auth.Userlogin({ queueId: Current.queueId, ...formState });
+      localStorage.setItem("queue_Id", Current.queueId);
+      localStorage.setItem("username", formState.username);
+      window.location.assign("/DisplayQueue");
     } catch (e) {
       window.location.assign("/ErrorPage");
     }
   };
 
   return (
-    <div className="text-center m-5-auto">
-      <h5>Enter your details below to view the estimate waiting time</h5>
-      <form onSubmit={handleFormSubmit}>
-        <p>
-          <label>username</label>
-          <br />
-          <input
-            type="username"
-            name="username"
-            value={formState.username}
-            onChange={handleChange}
-            required
-            placeholder="eg: John"
-          />
-        </p>
-        <p>
-          <label>Email address</label>
-          <br />
-          <input
-            type="email"
-            name="email"
-            value={formState.email}
-            onChange={handleChange}
-            required
-            placeholder="eg: johndoe@email.com"
-          />
-        </p>
-        <p>
-          <label>Phone number</label>
-          <br />
-          <input
-            type="phone"
-            name="phone"
-            value={formState.phone}
-            onChange={handleChange}
-            required
-            placeholder="eg: 647-647-6476"
-          />
-        </p>
-        <p>
-          <input type="checkbox" name="checkbox" id="checkbox" required />{" "}
-          <span>
-            I agree all statements in{" "}
-            <a
-              href="https://google.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              terms of service
-            </a>
-          </span>
-          .
-        </p>
-        <p>
-          <button className="btn d-block w-100" type="submit">
-            Submit
-          </button>
-        </p>
-      </form>
-      <footer>{/* <p><Link to="/">Back to HomePage</Link>.</p> */}</footer>
-    </div>
+    <main>
+      <Nav />
+      <div className="text-center m-5-auto">
+        <h5>Enter your details below to view the estimate waiting time</h5>
+        <form onSubmit={handleFormSubmit}>
+          <p>
+            <label>username</label>
+            <br />
+            <input
+              type="username"
+              name="username"
+              value={formState.username}
+              onChange={handleChange}
+              required
+              placeholder="eg: John"
+            />
+          </p>
+          <p>
+            <label>Email address</label>
+            <br />
+            <input
+              type="email"
+              name="email"
+              value={formState.email}
+              onChange={handleChange}
+              required
+              placeholder="eg: johndoe@email.com"
+            />
+          </p>
+          <p>
+            <label>Phone number</label>
+            <br />
+            <input
+              type="phone"
+              name="phone"
+              value={formState.phone}
+              onChange={handleChange}
+              required
+              placeholder="eg: 647-647-6476"
+            />
+          </p>
+          <p>
+            <input type="checkbox" name="checkbox" id="checkbox" required />{" "}
+            <span>
+              I agree all statements in{" "}
+              <a
+                href="https://google.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                terms of service
+              </a>
+            </span>
+            .
+          </p>
+          <p>
+            <button className="btn d-block w-100" type="submit">
+              Submit
+            </button>
+          </p>
+        </form>
+        <footer>{/* <p><Link to="/">Back to HomePage</Link>.</p> */}</footer>
+      </div>
+      <Footer />
+    </main>
   );
 }
